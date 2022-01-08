7fe90788b5a34ac7bc29786ba457e3e337189a7a
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-01-08 12:01:28 +0200

    Add project files.

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass test = new TestClass();
+            FieldInfo[] instanceFields = typeof(TestClass)
+                .GetFields(
+                         BindingFlags.NonPublic |
+                         BindingFlags.Instance);
+            FieldInfo[] staticFields = typeof(TestClass)
+                .GetFields(
+                     BindingFlags.NonPublic |
+                     BindingFlags.Static);
+
+            foreach (var field in instanceFields)
+            {
+                field.SetValue(test, 10);
+                Console.WriteLine(field + ":" + field.GetValue(test));
+            }
+
+            foreach (var field in staticFields)
+            {
+                Console.Write(field + ":");
+                Console.WriteLine(field.GetValue(null));
+            }
+        }
+    }
+}
---
+++ ReflectionDemo/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    public class TestClass
+    {
+        private int A;//
+        protected int B;//
+        public int C;
+
+        public static int D;
+        protected static int E;//
+        private static int F;//
+
+        public TestClass()
+        {
+            A = 1;
+            B = 2;
+            C = 3;
+        }
+
+        static TestClass()
+        {
+            D = 5;
+            E = 6;
+            F = 7;
+        }
+    }
+}
